8aa6f2e7cbe3e859e8cbb1c2105d44eb074b0fd9
abeyk <abey@Abeys-MBP.lan>
2023-11-21 18:37:01 -0500

    better flip animation

--- js/script.js
+++ js/script.js
@@ -11,10 +11,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     anime({
       targets: profileCard,
+      opacity: ['100%', '100%'],
       translateX: ['-50%', '-50%'],
       translateY: ['-50%', '-50%'],
-      scale: [{ value: 1, easing: 'easeInOutSine' }, { value: 1.15, easing: 'easeInOutSine', delay: 250 }, { value: 1, easing: 'easeInOutSine', delay: 250 }],
-      rotateY: { value: '+=180', easing: 'easeInOutSine', delay: 250 },
+      scale: [{ value: 0.85, easing: 'easeInOutSine', delay: 100 }, { value: 1.05, easing: 'easeInOutSine', delay: 300 }, { value: 1, easing: 'easeInOutSine' }],
+      rotateY: { value: '+=180', easing: 'easeInOutSine', delay: 150 },
       easing: 'easeInOutSine',
       duration: 600,
       complete: function (anim) {
