b8a2d39d58ae76a0d6693a4ddf3c0247f0f6b77b
Abey Koolipurackal <hello@abeyk.com>
2023-11-23 11:26:32 -0500

    css

--- js/script.js
+++ js/script.js
@@ -12,12 +12,25 @@ document.addEventListener('DOMContentLoaded', function () {
     anime({
       targets: profileCard,
       opacity: ['100%', '100%'],
+
       translateX: ['-50%', '-50%'],
       translateY: ['-50%', '-50%'],
-      scale: [{ value: 1, easing: 'easeInOutSine', delay: 0 }, { value: .95, easing: 'easeInOutSine', delay: 100 }, { value: 1, easing: 'easeInOutSine', delay: 0 }],
-      rotateY: { value: '+=180', easing: 'easeInOutSine', delay: 20 },
+      scale: [{ value: 0.95, easing: 'easeInOutSine', delay: 0 }, { value: 1.05, easing: 'easeInOutSine', delay: 0 }, { value: 1, easing: 'easeInOutSine', delay: 0 }],
+      // rotateX: { value: '+=180', easing: 'easeInOutSine', delay: 10 },
+      skewY: [{ value: '+=4', easing: 'easeInOutSine', delay: 10 }, 0],
+      skewX: [{ value: '+=4', easing: 'easeInOutSine', delay: 10 }, 0],
+      rotateY: { value: '-=180', easing: 'easeInOutSine', delay: 10 },
+      // rotateX: { value: '+=180', easing: 'easeInOutSine', delay: 10 },
+      // rotateY: [0, 180],
+      // rotateX: [0, 360],
+      // rotateX: { value: '-=10', easing: 'easeInOutSine', delay: 10 },
+      // rotateY: 180,
+      // scale: [0.9, 1.2, 1],
+      // translateX: 300,
+      
+      // rotateX: { value: '+=480', easing: 'easeInOutSine', delay: 0 },
       easing: 'easeInOutSine',
-      duration: 800,
+      duration: 900,
       complete: function (anim) {
         playing = false;
       }
