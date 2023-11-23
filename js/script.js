2eb31829bfc60b4dc82146aab3f2573594fa1b5a
Abey Koolipurackal <hello@abeyk.com>
2023-11-23 02:13:14 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
       opacity: ['100%', '100%'],
       translateX: ['-50%', '-50%'],
       translateY: ['-50%', '-50%'],
-      scale: [{ value: 0.85, easing: 'easeInOutSine', delay: 50 }, { value: 1.05, easing: 'easeInOutSine', delay: 100 }, { value: 1, easing: 'easeInOutSine' }],
+      scale: [{ value: 0.5, easing: 'easeInOutSine', delay: 0 }, { value: 1.05, easing: 'easeInOutSine', delay: 100 }, { value: 1, easing: 'easeInOutSine' }],
       rotateY: { value: '+=180', easing: 'easeInOutSine', delay: 55 },
       easing: 'easeInOutSine',
       duration: 300,
